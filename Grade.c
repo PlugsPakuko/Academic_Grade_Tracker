db2ec56d0bab999542dd85994700eaefe1293cec
plugs <plugs2004@gmail.com>
2023-11-13 21:04:18 +0700

    Add Semesters grade

--- Grade.c
+++ Grade.c
@@ -28,7 +28,6 @@ char** split_line(char *line) {
     return splitted;
 }
 
-
 int get_subject_number(char *code, char code_list[Course_count][5]){
     for(int i = 0; i < Course_count; i++){
         if(strncmp(code_list[i], code, strlen(code_list[i]) - 1) == 0)
@@ -38,9 +37,9 @@ int get_subject_number(char *code, char code_list[Course_count][5]){
 }
 
 void save_result(char *file_name,char code_list[Course_count][5] ,Course *student_grade, double sum_grade, double sum_credit){
-    FILE* outFile = fopen(file_name,"w");
+    FILE* outFile = fopen(file_name,"a+");
 
-    fprintf(outFile,"Average Grade from Transcript.csv\n");
+    fprintf(outFile,"Overall Average Grade\n");
     fprintf(outFile,"Course,Credit,Average Grade\n");
     for(int i = 0; i < Course_count;i++){
         fprintf(outFile,"%s",code_list[i]);
@@ -65,6 +64,11 @@ int main(void){
     FILE* infile;
     infile = fopen("Transcript.csv", "r");
 
+    FILE *outfile;
+    outfile = fopen("Output.csv", "w");
+    fprintf(outfile,"Average grade per Semester\n");
+    fprintf(outfile,"Grade Semester,Credit,Average Grade\n");
+
     if(infile == NULL){
         perror("Error opening Input file\n");
         return 1;
@@ -73,6 +77,11 @@ int main(void){
     char line[100]; 
     fgets(line, sizeof(line), infile);
 
+    int sem = 1;
+    int grade = 10;
+    int termpass = 0;
+    double Sem_grade = 0;
+    double Sem_credit = 0;
     while (fgets(line,sizeof(line),infile))
     {
         char **token;
@@ -82,8 +91,13 @@ int main(void){
         if(line[0] == ',' && line[1] != ','){
             token = split_line(line);
             code_num = get_subject_number(token[0], Code);
+
             student_grade[code_num].credit += atof(token[2]);
             student_grade[code_num].grade += atof(token[3]) * atof(token[2]);
+
+            Sem_credit += atof(token[2]);
+            Sem_grade += atof(token[3]) * atof(token[2]);
+            termpass = 1;
         }
         //Grade_term line handle
         else if(line[0] != ','){
@@ -92,12 +106,48 @@ int main(void){
 
             student_grade[code_num].credit += atof(token[3]);
             student_grade[code_num].grade += atof(token[4]) * atof(token[3]);
+
+            Sem_credit += atof(token[3]);
+            Sem_grade += atof(token[4]) * atof(token[3]);
+            termpass = 1;
         }
         //empty line && Column name line handle
         else{
+            if(termpass == 1){
+                printf("Grade:%d ", grade);
+                printf("SEM:%d", sem);
+                printf("Average Grade: %.2lf\n\n", Sem_grade/Sem_credit);
+
+                fprintf(outfile,"Grade:%d ", grade);
+                fprintf(outfile,"Semester:%d,", sem);
+                fprintf(outfile,"%.2lf,%.2lf\n", Sem_credit, Sem_grade/Sem_credit);
+                Sem_credit = 0;
+                Sem_grade = 0;
+                sem++;
+                if(sem == 3){
+                    sem = 1;
+                    grade += 1;
+                }
+                termpass = 0;
+            }
             continue;
         }
     }
+    if(termpass){
+        printf("Grade:%d ", grade);
+        printf("SEM:%d", sem);
+        printf("Average Grade: %.2lf\n\n", Sem_grade/Sem_credit);
+
+        fprintf(outfile,"Grade:%d ", grade);
+        fprintf(outfile,"Semester:%d,", sem);
+        fprintf(outfile,"%.2lf,%.2lf\n", Sem_credit, Sem_grade/Sem_credit);
+        Sem_credit = 0;
+        Sem_grade = 0;
+        sem++;
+        termpass = 0;
+    }
+    fprintf(outfile,",,\n");
+    
 
     
     double sum_grade = 0;
